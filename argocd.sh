7bf2b4dc89f003fc8dd44d345d4c8190f8eb155e
Shikhrshukla <shikhar31690.1@gmail.com>
2025-09-21 16:17:48 +0530

    adding extra files

---
+++ argocd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Installing Argocd and setting namespaces...
+#kubectl create namespace argocd
+#kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+#Setting default namespace as "argocd"...
+#kubectl config set-context --current --namespace=argocd
+
+#Logging...
+argocd login --core
+
+#This is your "admin" password for the console
+argocd admin initial-password -n argocd
+
+#Destination Clusters
+argocd cluster list
