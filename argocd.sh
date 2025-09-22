e71cf99c49f0082fc489df759dca81c8c1925065
Shikhrshukla <shikhar31690.1@gmail.com>
2025-09-22 23:29:07 +0530

    helm changes

--- argocd.sh
+++ argocd.sh
@@ -4,6 +4,8 @@
 #kubectl create namespace argocd
 #kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+sleep(30)
+
 #Setting default namespace as "argocd"...
 #kubectl config set-context --current --namespace=argocd
 
